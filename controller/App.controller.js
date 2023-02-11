df38e3c2afa74128661cf6d7c2b3f8f678a76442
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2023-02-12 00:38:37 +0100

    Groundwork Tiles

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -7,6 +7,18 @@ sap.ui.define([
     onInit: function() {
       // apply content density mode to root view
       this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	  console.log("Geladen App")
+	  // You can choose the source of the apps to be listed by uncommenting the respective line
+	  // Search apps folder (simple)
+	  
+	  // Seach apps folder (and definition files in appfolders)
+	  
+	  // JSONmodel (saved in model folder)
+	  
+	  // Demodefinition below
+	  
+	  // Custom Source (i.e. from an API)
+	  
     }
   });
 
--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -6,9 +6,52 @@ sap.ui.define([
 
   return BaseController.extend("ui5.hft.portal.controller.Main", {
 	  
-	  
-    sayHello: function() {
-      MessageBox.show("Hello World!");
+    onSettingPress: function() {
+		console.log("Setting");
+		// Add a tile.
+		//var form = this.getView().byId(this.createId("grid1"));
+		
+		// Add a section (outer loop)
+		
+		// Add tiles (inner loop) 
+		// Define a new tile
+		var tile=new sap.m.GenericTile({
+		                    header : "text1",
+					   		subheader: "Minitest",
+					   	 	press: "notimplemented",
+					   		url: "#"
+		                })
+		tile.setLayoutData(new sap.f.GridContainerItemLayoutData({
+				minRows:2,
+				columns:2}));
+		// Add Icon and Footer to tile
+		tile.addTileContent(
+			new sap.m.TileContent({
+				footer: "footertext"
+			})
+			.setContent(new sap.m.ImageContent({
+				src: "sap-icon://time-overtime"
+				}))
+			);
+		// OR: add KPI and trend to tile
+		tile.addTileContent(
+			new sap.m.TileContent({
+				footer: "footertext",
+				unit: "EUR"
+			})
+			.setContent(new sap.m.NumericContent({
+				value: 1000,
+				scale: "M",
+				valueColor: "Error",
+				indicator: "Up" 
+				}))
+			);
+		console.log(tile);
+		var form = this.getView().byId("grid1");
+		form.addItem(tile)
+    },
+    onUserPress: function() {
+		console.log("User");
     }
   });
 
