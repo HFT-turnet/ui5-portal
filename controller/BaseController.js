dd8e5c6c158c35fe0e14f879aaf0f4736c115b5f
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2023-02-19 10:06:48 +0100

    Generate Apps at load

--- controller/BaseController.js
+++ controller/BaseController.js
@@ -45,26 +45,30 @@ sap.ui.define([
 	},
 
 	loadApps: function(appcollection, that){
+		// This is a function to fetch the apps from the appcollection and create the sections and tiles.
+		// The purpose of separating this function is to make it easier to change the source of the apps.
 		// take all "groups" within the apps array of objects
 		var groups=appcollection.map(function(app){return app.group});
 		// remove duplicates
 		var uniquegroups=groups.filter(function(item, pos) {
 			return groups.indexOf(item) == pos;
 		});
-		console.log(uniquegroups);
+		//console.log(uniquegroups);
 		// Create the sections according to the groups
 		uniquegroups.forEach(function(group){
-			console.log(group);
+			//console.log(group);
 			that.addSection(group.replace(/\s/g, ''),group,that);
 		});
 		// Create the Apps in the sections
 		appcollection.forEach(function(app){
-			console.log(app);
 			that.addTile(app.group.replace(/\s/g, ''),app,that);
 		});
 	},
 
 	addSection: function(sectionid, sectionname,that){
+		// This is a function to create a section in the portal
+		// It receives a new section id, a section name and the view reference to work independent from the initial view.
+		// A bar with the title and a grid container are created. The grid container holds the id of the section.
 		var bar= new sap.m.Bar();
 		bar.addContentLeft(new sap.m.Label( {text: sectionname}));
 		that.getView().byId("portal").addContent(bar);	
@@ -75,6 +79,8 @@ sap.ui.define([
 	},
 	
 	addTile: function(sectionid, tiledata, that){
+		// This is a function to create a tile in the portal in a specific section.
+		// It receives the section id, the tile data and the view reference to work independent from the initial view.
 		var tile=new sap.m.GenericTile({
 		                    header : tiledata.header,
 					   		subheader: tiledata.subheader,
