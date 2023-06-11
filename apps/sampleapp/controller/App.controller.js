370621f28b1696fe947f44a8d4b3096f3c0a0944
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2023-06-11 22:09:16 +0200

    Update App.controller.js

--- apps/sampleapp/controller/App.controller.js
+++ apps/sampleapp/controller/App.controller.js
@@ -5,31 +5,10 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("ui5.hft.portalappsample.controller.App", {
-    onInit: function() {
-	  // The app is generally designed as needed. The only section, that provides a bridge to the portal
-	  // Is this init function. It receives the token and the settings from the portal and stores them in a model.
-	
+    onInit: function() {	
       // apply content density mode to root view
       this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-	  
-	  // Check for available settings, tokens and data.
-	  var token=sessionStorage.token;
-	  //var settings = new JSONModel();
-	  //settings.setJSON(sessionStorage.xsettings);
-	  //console.log(settings);
-	  //this.getView().setModel(settings,"settings");
-	  
-	// SET absolute Portal Path for back-button.
 
-	  // Sources: localStorage and sessionStorage.
-	  
-	  //localStorage.ft_user="Test";
-	  //console.log(localStorage.ft_token);
-	  //console.log(sessionStorage.ft_secret);
-	  //console.log(localStorage.token);
-	  //console.log(sessionStorage.xmodel)
-
-	  //console.log(model);
     }
   });
 
